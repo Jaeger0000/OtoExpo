76a8b420e24579e378fa4b0443a429fbc0f9d9a7
Oguzhan Ceviz <oguzhanceviz68@gmail.com>
2024-03-24 14:25:05 +0300

    databese updated

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('otoExpo-databese','root','Oguzhan6886', {dialect: 'mysql' , host: 'localhost'});
+const sequelize = new Sequelize('OtoExpo_simplypast','OtoExpo_simplypast','c53cdce7a5a2a1b83af4202ad78f1e318a5246d2', {dialect: 'mysql' , host: 'dyy.h.filess.io', port: 3307});
 
 module.exports = sequelize;
